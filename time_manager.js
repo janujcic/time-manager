2b1ff05c82be8b9ba22e1ef70b3d699e14b9fca5
jujcic <janox233@gmail.com>
2024-11-14 21:07:33 +0100

    A table for saved task times.

--- time_manager.js
+++ time_manager.js
@@ -3,16 +3,28 @@ const confirmationModal = document.getElementById("confirmation-modal");
 document.addEventListener("DOMContentLoaded", function () {
   getSessionsFromBackground((sessions) => {
     const logDisplay = document.getElementById("log-display");
+    const logTableBody = document.querySelector("#task-log-table tbody");
+
     sessions.sort((a, b) => b.duration - a.duration);
     if (sessions.length === 0) {
       logDisplay.innerText = "No recorded sessions yet.";
     } else {
       sessions.forEach((session, index) => {
-        const sessionEntry = document.createElement("p");
-        sessionEntry.innerText = `Task ${index + 1}: ${
-          session.task
-        } - ${transformMilisecondsToTime(session.duration)}`;
-        logDisplay.appendChild(sessionEntry);
+        const row = document.createElement("tr");
+        const taskNumberCell = document.createElement("td");
+
+        taskNumberCell.innerText = index + 1;
+        row.appendChild(taskNumberCell);
+
+        const taskNameCell = document.createElement("td");
+        taskNameCell.innerText = session.task;
+        row.appendChild(taskNameCell);
+
+        const durationCell = document.createElement("td");
+        durationCell.innerText = transformMilisecondsToTime(session.duration);
+        row.appendChild(durationCell);
+
+        logTableBody.appendChild(row);
       });
     }
   });
@@ -36,6 +48,8 @@ document.getElementById("yes-button").addEventListener("click", () => {
   clearSessionsInBackground();
   document.getElementById("log-display").innerText = "Log cleared."; // Update display
   confirmationModal.style.display = "none"; // Hide modal
+  const logTableBody = document.querySelector("#task-log-table tbody");
+  logTableBody.innerHTML = ""; // Remove all rows from the table
 });
 
 document.getElementById("no-button").addEventListener("click", () => {
